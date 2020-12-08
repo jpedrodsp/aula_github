4a2126989215ad078bd27aeb4f00de1b4000bead
João Pedro Patrocinio <jpedro9966@gmail.com>
2020-12-08 18:07:50 -0300

    Storing clients data in top-level function #6

--- Main.java
+++ Main.java
@@ -3,6 +3,9 @@ import java.util.Arrays;
 public class Main {
 
 	public static void main(String[] args) {
+		// This is our temporary way of storing client data in our program
+		Dictionary<String, Client> clients = new Hashtable<String, Client>();
+		
 		Menu mainMenu =  new Menu("Menu Principal", Arrays.asList("Conta", "Cliente", "Operacoes"));
 		System.out.println(mainMenu.getSelection() + "foi selecionada");
 		System.out.println("Fim");
